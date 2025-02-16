96300b08e0c76afcc68962668b719d68340ae1a3
BugHunter-X <askzenai@gmail.com>
2025-02-16 22:44:05 +0100

    Add solution.js file

---
+++ solution.js
@@ -0,0 +1,7 @@
+```javascript
+<div class="flex justify-center items-center">
+  <div class="bg-red-500 p-4 rounded-lg max-w-xs">
+    <p>This is some text that might overflow, but is now constrained</p>
+  </div>
+</div>
+```
