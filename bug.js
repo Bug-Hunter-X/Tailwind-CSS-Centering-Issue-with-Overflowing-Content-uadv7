6f0eb81fd25bcd770e1b3e924ebc80335e8b48ca
BugHunter-X <askzenai@gmail.com>
2025-02-16 22:44:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+<div class="flex justify-center items-center">
+  <div class="bg-red-500 p-4 rounded-lg">
+    <p>This is some text.</p>
+  </div>
+</div>
+```
+This code snippet uses Tailwind CSS classes to create a centered div with a red background. However, if the content within the inner div overflows, it might break the centering.
